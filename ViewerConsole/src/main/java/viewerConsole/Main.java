919ff4c03e65d615227322b7c5de94c49e96ccfd
taljmars <taljmars@gmail.com>
2017-03-04 17:53:11 +0200

    maven repo

--- ViewerConsole/src/main/java/viewerConsole/Main.java
+++ ViewerConsole/src/main/java/viewerConsole/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application  {
 	
 	@Override
     public void start(Stage primaryStage) {
-        Parent root = (Parent) AppConfig.loader.load("view.fxml");
+        Parent root = (Parent) AppConfig.loader.load("views/view.fxml");
 		root.setStyle("-fx-background-color: whitesmoke;");
 		Scene scene = new Scene(root, 650, 550);
         primaryStage.setResizable(false);
